0f68d3ba75a7703ad4bf68c84bbe7f76953bf7f4
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2025-05-04 22:23:26 +0600

    update

---
+++ config/constants.tsx
@@ -0,0 +1,9 @@
+import { Image } from "react-native";
+
+export const onBoardingSlides: onBoardingSlidesTypes[] = [
+  {
+    color: "#40e0d0",
+    title: "Explore",
+    image: <Image source={One} style={{}} />,
+  },
+];
--- screens/onboarding/onboarding.screen.tsx
+++ screens/onboarding/onboarding.screen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 const OnboardingScreen = () => {
   return (
@@ -8,5 +8,7 @@ const OnboardingScreen = () => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({});
 
 export default OnboardingScreen;
